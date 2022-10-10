5d7c7489d178031c53e1b4eead7299877f6f4b6e
Krai53n <greasha46@gmail.com>
2022-10-10 16:28:38 +0300

    update emacs dots

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -2,18 +2,25 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+
 (global-display-line-numbers-mode 1)
+(column-number-mode 1)
 
-(setq inhibit-startup-message t)
-(setq display-line-numbers-type 'relative)
+(evil-mode 1)
 
-(custom-set-variables '(custom-enabled-themes '(gruvbox-dark-hard))
-		      '(custom-safe-themes
-			'("d80952c58cf1b06d936b1392c38230b74ae1a2a6729594770762dc0779ac66b7" default))
-		      '(package-archives
-			'(("gnu" . "https://elpa.gnu.org/packages/")
-			  ("melpa" . "https://melpa.org/packages/")))
-		      '(package-selected-packages '(gruvbox-theme)))
+(setq
+ inhibit-startup-message t
+ display-line-numbers-type 'relative
+ make-backup-files nil
+ default-directory "c:/Users/Юзер/Desktop"
+ fill-column 79)
 
-(add-to-list 'default-frame-alist
-	     '(font . "JetBrains Mono NL-10"))
+(add-to-list
+ 'default-frame-alist
+ '(font . "JetBrains Mono-10"))
+
+(custom-set-variables
+ '(custom-enabled-themes '(kanagawa))
+ '(package-selected-packages
+   '(md-readme markdown-preview-mode haskell-mode doom-themes evil
+     gruvbox-theme)))
